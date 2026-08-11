e59582765909539b4f07ec35bdbbe2a74b7ed8b1
cookie729 <cookie.web.729@gmail.com>
2026-08-11 10:30:42 +0900

    feat: create swiper

--- js-practice2/js/slider.js
+++ js-practice2/js/slider.js
@@ -6,12 +6,21 @@ const nextButton = document.querySelector('.next');
 const dots = document.querySelectorAll('.dot');
 
 // 現在表示中の画像のインデックス
+// sliderImages.length = 5
+// js は 0 が1 始まり
 let currentIndex = 0;
 
 // トラックを現在のインデックス分だけ左右にずらして、1秒かけてスライドさせる
 const updateSlider = () => {
+  // currentIndex.length = 4 → 500%
+  // currentIndex = 画像の○枚目
+  // そこに 100 を掛けることで translateX の何%かを出している
+  // 例えば 3枚目なら * 100 で 300%。300%横にずらすことで、3枚目が表示される
   sliderTrack.style.transform = `translateX(-${currentIndex * 100}%)`;
 
+  // forEach はループ処理
+  // 今選択している index番目の と currentIndex が同じであれば active = true になる
+  // その結果、洗濯中の画像に対する dot だけ active になる
   dots.forEach((dot, index) => {
     dot.classList.toggle('active', index === currentIndex);
   });
@@ -21,6 +30,9 @@ const updateSlider = () => {
 // 次へボタンがクリックされたとき
 nextButton.addEventListener('click', () => {
   // 現在のインデックスに+1したインデックスが呼ばれる
+  // js では 0 は 1 ~
+  // 最後になると 4 + 1 = 5 % sliderImages.length = 5 のため、0。0 = 1 で最初に戻る
+  // この処理がないと 5枚目の次がないのに 5枚目を表示しようとしてバグる
   currentIndex = (currentIndex + 1) % sliderImages.length;
   // updateSlider が呼ばれ、トラックを現在のインデックス分だけ左右にずらして、1秒かけてスライド
   // dots も現在のindex 番号に合わせて active を更新する。 index currentIndex になる様にする
@@ -29,6 +41,9 @@ nextButton.addEventListener('click', () => {
 
 // 前の画像へ(最初の画像の前は最後に戻る)
 prevButton.addEventListener('click', () => {
+  // 今が 2
+  // 0 の時は 1 - 1 + 1 = 1
+  // 1 % 1 = 1 で 1 ページに移動する
   currentIndex = (currentIndex - 1 + sliderImages.length) % sliderImages.length;
   updateSlider();
 });
